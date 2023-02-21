8fe2b32fbb5e123599e9fab7f4ffb87529119929
flaming65 <namank6510@gmail.com>
2023-02-21 16:33:50 +0530

    first commit

---
+++ test/api.test.js
@@ -0,0 +1,10 @@
+const axios = require('axios');
+API_URL = "http://localhost:5000/api"
+test('test device array', () => {
+    axios.get(`${API_URL}/devices`)
+        .then(resp => resp.data)
+        .then(resp => {
+            // console.log(resp[1]);
+            expect(resp[0].user).toEqual('not-a-user');;
+        });
+});
